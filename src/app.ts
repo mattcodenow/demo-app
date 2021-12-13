dcd4e8df539f072bbe8975e5d62330aec165cc6c
Matthew Stewart <mattcodenow@gmail.com>
2021-12-12 20:52:23 -0800

    add node redis

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("works");
+import { createClient } from 'redis';
+
+(async () => {
+  // by default the websocket will connect to localhost:6379
+  // happens to be the config for our redis container
+  const client = createClient();
+
+  // when an error happens, log it to console
+  client.on('error', (err) => console.log('Redis Client Error', err));
+  
+  // measuring connection execution time
+  console.time("redis-connection");
+  
+  // connect to the redis container
+  await client.connect();
+
+  // finish connection timer and log
+  console.timeEnd("redis-connection");
+
+  // log whether the connection is open
+  console.log("Connected: ", client.isOpen);
+
+  //await client.set('key', 'value');
+  //const value = await client.get('key');
+  //console.log(value);
+  setTimeout(() => client.quit(), 5000);
+})();
